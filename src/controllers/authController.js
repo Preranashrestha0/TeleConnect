92630c26a8a1a8c5e86ee115a90b68a03566d37d
Preranashrestha0 <preranas767@gmail.com>
2024-08-02 08:09:30 +0545

    new backend features

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,48 +1,65 @@
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const User = require("../models/authUserModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const UserProfile = require("../models/userProfile");
 
 dotenv.config();
 
 const registerUser = async (req, res) => {
-  const { name, email, password , address, gender, age, role} = req.body;
+  const { name, email, password,role } = req.body;
+  // const userEmail = req.body.email;
 
   try {
-    let user = await User.findOne({email:email});
+    let user = await User.findOne({ email });
 
     if (user) {
-      return res.status(400).json({ msg: 'User already exists' });
+      return res.status(400).json({ msg: "User already exists" });
     }
 
     user = new User({
-      name,
-      email,
-      password,
-      address,
-      gender,
-      age,
-      role,
-      // name: req.body.name,
-      // name:name(leftside= databse field, and right side= enter value field)
+      // name,
+      // email,
+      // password,
+      name: name,
+      email: email,
+      password: password,
+      role:role
     });
 
     await user.save();
 
-    //information loading = payload
-//
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-//jwt uses to generate token
-//buffer is a space that holds a  data
-    
+    // Create profile for the new user
+    const newProfile = new UserProfile({ user: user._id });
+    await newProfile.save();
+
+    // do this if you want to redirect to dashboard after registration
+    // const payload = {
+    //   user: {
+    //     id: user.id,
+    //   },
+    // };
+
+    // console.log(payload);
+
+    // jwt.sign(
+    //   payload,
+    //   process.env.JWT_SECRET,
+    //   { expiresIn: "1h" },
+    //   (err, token) => {
+    //     if (err) throw err;
+    //     res.json({ token});
+    //   }
+    // );
+
+    res.status(201).json({
+      msg: "User registered successfully",
+      user: user,
+      userProfile: newProfile,
+    });
   } catch (err) {
     console.error(err.message);
-    // res.status(500).send(err.message);
-    res.status(500).send('Server error');
+    res.status(500).send({ msg: err.message });
   }
 };
 
@@ -53,13 +70,13 @@ const loginUser = async (req, res) => {
     let user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ msg: 'Invalid credentials' });
+      return res.status(400).json({ msg: "Invalid credentials" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({ msg: 'Invalid credentials' });
+      return res.status(400).json({ msg: "Invalid credentials" });
     }
 
     const payload = {
@@ -71,15 +88,19 @@ const loginUser = async (req, res) => {
     jwt.sign(
       payload,
       process.env.JWT_SECRET,
-      { expiresIn: '1h' },
+      { expiresIn: "1h" },
       (err, token) => {
         if (err) throw err;
-        res.json({ token });
+        res.json({
+          msg: "user logged in successfully",
+          token: `Bearer ${token}`,
+          user: user,
+        });
       }
     );
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server error');
+    res.status(500).send("Server error");
   }
 };
 
